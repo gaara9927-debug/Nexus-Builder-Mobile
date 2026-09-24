6bb754c15b51b393d0a3fd10855c8a08cd71216c
gaara9927-debug <gaara9927@gmail.com>
2026-09-23 23:30:05 -0300

    Add buildable Nexus Android application

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android {
+ namespace="com.nexus.builder"
+ compileSdk=35
+ defaultConfig { applicationId="com.nexus.builder"; minSdk=26; targetSdk=35; versionCode=1; versionName="0.1.0" }
+}
